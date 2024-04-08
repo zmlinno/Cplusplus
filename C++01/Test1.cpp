1cf1e2bbdd2ac357638cf1d8cef8f4260860c1ce
zmlinno <131747526+zmlinno@users.noreply.github.com>
2024-04-08 23:51:10 +0900

    Add files via upload

---
+++ C++01/Test1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//流提取
+int main()
+{
+
+    cout << "输入两个数字:" << endl;
+    int a,b;
+    cin >> a >> b;
+    
+    cout << "a->" << a << endl;
+    cout << "b->" << b << endl;
+    return 0;
+    //这是流提取，
+    //和scanf一样，输入2和3以后就输出。
+}
