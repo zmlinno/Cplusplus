4befeb34c94670aa9d4bbe8539dec712841035be
zmlinno <131747526+zmlinno@users.noreply.github.com>
2024-05-27 22:24:42 +0900

    string

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Test01
+//
+//  Created by 张木林 on 5/27/24.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1;
+    //cin >> s1;
+    string s2 = "hello,",s3 = "world\n";
+    string s4 = s2+s3;
+    cout << s4 << endl;
+    return 0;
+}
